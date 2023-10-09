d96652f0e8c87f5ea959e115ff5bf10994a875d0
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-09 09:50:24 +0200

    now ''Fizz-Buzz" is displayed in a orange box

--- js/master.js
+++ js/master.js
@@ -1,13 +1,22 @@
-for (let i = 0; i < 101 ; i++) {
-    
-    if (i % 15 === 0) {
+for (let i = 1; i < 101 ; i++) {
+    let n
+    n = i
+    if (n % 15 === 0) {
         console.log("FizzBuzz");
-    } else if (i % 5 === 0) {
+        n = 'FizzBuzz'
+    } else if (n % 5 === 0) {
         console.log("Buzz");
-    } else if (i % 3 == 0){
+        n = 'Buzz'
+    } else if (n % 3 == 0){
         console.log("Fizz");
+        n = 'Fizz'
     } else {
-        console.log (i);
+        console.log (n);
+        n = n
     }
 
+    let boxString = '<div class="box padding-right ">' + n + '</div>'
+    document.getElementById('container-big').innerHTML += boxString
 }
+
+
